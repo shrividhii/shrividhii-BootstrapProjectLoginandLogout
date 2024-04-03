05e7201e755c644408539599438d15d4c8f5124f
Vidhi Shrivastava <111991093+shrividhii@users.noreply.github.com>
2024-04-03 20:15:05 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project_vidhi.Models;
+using Newtonsoft.Json;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+
+namespace project_vidhi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult Check()
+        {
+            Users users = new Users()
+            {
+                Username = "vidhi", Password = "vidhi1234"
+
+            };
+
+            var json = JsonConvert.SerializeObject(users);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult ValidateCredentials(string returnUrl)
+        {
+            
+            var json = System.IO.File.ReadAllText("credentials.json");
+        //    var credentials = JsonSerializer.Deserialize<Users>
+        //(json);
+
+        //    if (Username == "testuser" && Password == "testpassword")
+        //    {
+        //        return Redirect(returnUrl);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Error = "Invalid credentials";
+                return View("Index");
+        //    }
+        }
+        public class Users
+        {
+            [Required]
+            public String Username { get; set; }
+
+            [Required]
+            public String Password { get; set; }
+        }
+    }
+}
---
+++ UserController.cs
@@ -0,0 +1,49 @@
+﻿using project_vidhi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project_vidhi.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Submit(LoginAuthentication model)
+        {
+            LoginAuthentication la = new LoginAuthentication();
+            la.Username = model.Username;
+            la.Password = model.Password;
+
+            if(la.Username=="vidhi" && la.Password=="vidhi1234")
+                return RedirectToAction("UserProfile","UserController");
+            else if(la.Username=="admin" &&  la.Password=="admin")
+                return RedirectToAction("AdminProfile","UserController");
+            return RedirectToAction("Index","UserController");     
+        }
+        public ActionResult UserProfile()
+        {
+            
+            return View();
+        }
+
+        public ActionResult AdminProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
